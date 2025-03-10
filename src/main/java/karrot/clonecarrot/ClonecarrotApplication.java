61d07ab0f516d39175a74e18e8c5bb06e1c1fd38
ohseongbin <dannybin814@naver.com>
2025-03-10 21:36:40 +0900

    init: 프로젝트 생성

---
+++ src/main/java/karrot/clonecarrot/ClonecarrotApplication.java
@@ -0,0 +1,13 @@
+package karrot.clonecarrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonecarrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonecarrotApplication.class, args);
+	}
+
+}
